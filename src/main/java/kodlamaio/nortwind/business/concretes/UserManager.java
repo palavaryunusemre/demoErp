fedf158272d4c48315115d4458d89aa4ed3008eb
palavaryunusemre <yunusemrepalavar.98@gmail.com>
2024-04-03 23:39:13 +0300

    user commit

---
+++ src/main/java/kodlamaio/nortwind/business/concretes/UserManager.java
@@ -0,0 +1,27 @@
+package kodlamaio.nortwind.business.concretes;
+
+import kodlamaio.nortwind.business.abstracts.UserService;
+import kodlamaio.nortwind.core.dataAccess.UserDao;
+import kodlamaio.nortwind.core.entities.User;
+import kodlamaio.nortwind.core.utilities.results.DataResult;
+import kodlamaio.nortwind.core.utilities.results.Result;
+import kodlamaio.nortwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.nortwind.core.utilities.results.SuccessResult;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserManager implements UserService {
+    private UserDao userDao;
+    public UserManager(UserDao userDao) {
+        this.userDao = userDao;
+    }
+    @Override
+    public Result add(User user) {
+        this.userDao.save(user);
+        return new SuccessResult("Kullanıcı eklendi");
+    }
+    @Override
+    public DataResult<User> findByEmail(String email) {
+        return new SuccessDataResult<User>(this.userDao.findByEmail(email),"Kullanıcı bulundu");
+    }
+}
