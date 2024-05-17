ef07ea5df2e83abaf9d0cd91528210678e024864
palavaryunusemre <yunusemrepalavar.98@gmail.com>
2024-05-17 14:47:33 +0300

    signUp-fixed

---
+++ src/main/java/kodlamaio/nortwind/core/utilities/exceptions/UniqueValueException.java
@@ -0,0 +1,7 @@
+package kodlamaio.nortwind.core.utilities.exceptions;
+
+public class UniqueValueException extends RuntimeException {
+    public UniqueValueException(String message) {
+        super(message);
+    }
+}
