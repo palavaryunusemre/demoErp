44cf50ad87d26ffff13e9ceeb6dc3aa184769062
palavaryunusemre <yunusemrepalavar.98@gmail.com>
2024-05-21 12:14:28 +0300

    userDto

---
+++ src/main/java/kodlamaio/nortwind/entities/dtos/UserDto.java
@@ -0,0 +1,15 @@
+package kodlamaio.nortwind.entities.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private UUID id;
+    private String name;
+}
